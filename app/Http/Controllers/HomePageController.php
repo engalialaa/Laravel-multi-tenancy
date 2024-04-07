66e71ed9fb22235c48d96f12b2575ddf3e06b79b
engalialaa <alialaaelruba@gmail.com>
2024-04-07 13:55:43 +0200

    first commit

---
+++ app/Http/Controllers/HomePageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Service\Tenants;
+use Illuminate\Http\Request;
+
+class HomePageController extends Controller
+{
+    public function index(){
+
+       return  Tenants::getTenant();
+       $users = User::get();
+       return view('welcome' , compact('users'));
+    }
+}
---
+++ app/Http/Middleware/TenantsMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Tenant;
+use App\Service\Tenants;
+use App\Service\TenantServcie;
+
+class TenantsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $host = $request->getHost();
+        $tenant = Tenant::where('domain',$host)->first();
+        Tenants::switchToTenant($tenant);
+        return $next($request);
+    }
+}
